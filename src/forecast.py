0fbbed96d6d3ef13014aaae9fdb0de8f7ae24943
sheng.gao <goseng123@gmail.com>
2025-06-20 22:02:18 +0800

    regression done

--- src/forecast.py
+++ src/forecast.py
@@ -11,6 +11,11 @@ from questdb.ingress import Sender, TimestampNanos
 import requests
 import pandas as pd
 from datetime import datetime
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import mean_absolute_error, root_mean_squared_error, r2_score
+import json
+import pickle
 
 rpth = Path(os.path.abspath(__file__)).parent.parent/"src"
 sys.path.append(str(rpth))
@@ -20,19 +25,34 @@ for  v in rpth.iterdir():
 
 import qstdb
 
-def lgb_train_model(train_x, train_y, train_df, test_x, test_df, model_params, target):
+def lgb_train_model(train_x, train_y, df_train, test_x, test_y, df_test, dev_x, dev_y, df_dev, model_params, target, checkpoint_name = None):
     lgb_train = lgb.Dataset(train_x, label = train_y, categorical_feature=model_params.get('categorical_feature', []))
     params = deepcopy(model_params)
-    model = lgb.train(params, lgb_train, num_boost_round=100)
+    valid_data = lgb.Dataset(dev_x, label=dev_y, reference=lgb_train)  
 
-    test_df['prediction'] = model.predict(test_x)
-    test_df['acutual'] = test_df["log_return"]
+    model = lgb.train(params, 
+        lgb_train, 
+        num_boost_round = 1000,
+        valid_sets=[valid_data],
+        valid_names=['valid'],
+        callbacks=[
+                lgb.early_stopping(stopping_rounds = 20),
+                lgb.log_evaluation(10)  # Print metrics every 10 rounds
+            ]                      
+    )
 
-    result = test_df[['Date', 'Ticker', target, 'prediction', 'acutual']]
+    df_test[f"{target}_log"] = df_test[target]
+    df_test['predict_log'] = model.predict(test_x)
+    df_test['predict'] = df_test['predict_log'].apply(lambda x: int(np.exp(x)))
 
-    return result
+    if checkpoint_name:
+        model.save_model(checkpoint_name)
 
-def features():
+    return df_test
+
+def train_eval(testid = "regres_lgb_20250620", checkpoint_path = "./checkpoint"):
+    checkpoint_name = os.path.join(checkpoint_path, f"{testid}.model")
+
     sql_str = """SELECT * FROM hdb_resale_transactions;"""
     df = qstdb.query(sql_str)
     df["month"] = df["month"].apply(lambda x: datetime.strptime(x, "%Y-%m"))
@@ -74,7 +94,8 @@ def features():
             y2 = round(y2 / 12, 2)
         except:
             y2 = 0
-        return y + y2
+        y = y + y2
+        return y 
     def flat_type(s):
         if s == "MULTI GENERATION":
             return "MULTI-GENERATION"
@@ -84,6 +105,8 @@ def features():
     df["remaining_lease"] = df["remaining_lease"].apply(lambda x: rem_lease(x))
     df["flat_type"] = df["flat_type"].apply(lambda x: flat_type(x))
     df.dropna(how = "any", inplace=True)
+    df = df[df["remaining_lease"] > 0]
+    df["remaining_lease"] = df["remaining_lease"].apply(np.log)
 
     #split
     r = [0.7, 0.1, 0.2]
@@ -96,50 +119,109 @@ def features():
     }
 
     #Train set
-    df_train = df[df["month"].isin(split_dt["train"])][feat_cols]
-    df_dev = df[df["month"].isin(split_dt["dev"])][feat_cols]
-    df_test = df[df["month"].isin(split_dt["test"])][feat_cols]
+    df_train = df[df["month"].isin(split_dt["train"])]
+    df_dev = df[df["month"].isin(split_dt["dev"])]
+    df_test = df[df["month"].isin(split_dt["test"])]
 
     #flat type
-    flat_set = dict(map(lambda x: (x[0], x[1]), enumerate(df_train["flat_type"].unique())))
+    twon_set = dict(map(lambda x: (x[1], x[0] + 1), enumerate(df_train["town"].unique())))
+    df_train["town_tag"] = df_train["town"].apply(lambda x: twon_set.get(x, None))
+    df_dev["town_tag"] = df_dev["town"].apply(lambda x: twon_set.get(x, None))
+    df_test["town_tag"] = df_test["town"].apply(lambda x: twon_set.get(x, None))
 
-    print(f"{df_train["flat_type"].unique()}, ** {df_train["storey_range"].unique()}, ** {df_train["flat_model"].unique()}")
-    print(f"{df_dev["flat_type"].unique()}, ** {df_dev["storey_range"].unique()}, ** {df_dev["flat_model"].unique()}")
-    print(f"{df_test["flat_type"].unique()}, ** {df_test["storey_range"].unique()}, ** {df_test["flat_model"].unique()}")
+    flat_set = dict(map(lambda x: (x[1], x[0] + 1), enumerate(df_train["flat_type"].unique())))
+    df_train["flat_type_tag"] = df_train["flat_type"].apply(lambda x: flat_set.get(x, None))
+    df_dev["flat_type_tag"] = df_dev["flat_type"].apply(lambda x: flat_set.get(x, None))
+    df_test["flat_type_tag"] = df_test["flat_type"].apply(lambda x: flat_set.get(x, None))
 
-    return {"train": df_train, "dev": df_dev, "test": df_test}
+    df_train["floor_area_sqm"] = df_train["floor_area_sqm"].apply(np.log)
+    df_dev["floor_area_sqm"] = df_dev["floor_area_sqm"].apply(np.log)
+    df_test["floor_area_sqm"] = df_test["floor_area_sqm"].apply(np.log)
 
-features()
+    df_train["resale_price_raw"] = df_train["resale_price"].copy()
+    df_dev["resale_price_raw"] = df_dev["resale_price"].copy()
+    df_test["resale_price_raw"] = df_test["resale_price"].copy()
+    df_train["resale_price"] = df_train["resale_price"].apply(np.log)
+    df_dev["resale_price"] = df_dev["resale_price"].apply(np.log)
+    df_test["resale_price"] = df_test["resale_price"].apply(np.log)
 
-def train():
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    feat_meta = {
+        "cat": ["storey_range", "town_tag", "flat_type_tag"],
+        "num": ["floor_area_sqm", "remaining_lease"],
+        "target": "resale_price",
+    }
+    #
+    scaler = StandardScaler()
+    train_x_num = df_train[feat_meta["num"]]
+    train_x_scaled_num = scaler.fit_transform(train_x_num)
+    train_y = df_train[feat_meta["target"]]
+    
+    dev_x_num = df_dev[feat_meta["num"]]
+    dev_x_scaled_num = scaler.transform(dev_x_num)
+    dev_y = df_dev[feat_meta["target"]]
 
-    model = xgb.XGBRegressor(
-        n_estimators=1000,
-        learning_rate=0.01,
-        max_depth=6,
-        subsample=0.8,
-        colsample_bytree=0.8,
-        early_stopping_rounds=10,
-        random_state=42
-    )
+    test_x_num = df_test[feat_meta["num"]]
+    test_x_scaled_num = scaler.transform(test_x_num)
+    test_y = df_test[feat_meta["target"]]
 
-    model.fit(X_train, y_train, eval_set=[(X_test, y_test)], verbose=True)
+    # Train the model
+    train_x_scaled_num = pd.DataFrame(train_x_scaled_num, columns=feat_meta["num"])
+    dev_x_scaled_num = pd.DataFrame(dev_x_scaled_num, columns=feat_meta["num"])
+    test_x_scaled_num = pd.DataFrame(test_x_scaled_num, columns=feat_meta["num"])
 
-    params = {
-        'max_depth': [4, 6, 8],
-        'learning_rate': [0.01, 0.1, 0.2],
-        'n_estimators': [500, 1000, 1500]
+    train_x_scaled = pd.concat([train_x_scaled_num, df_train[feat_meta["cat"]].reset_index(drop=True)], axis=1)
+    dev_x_scaled = pd.concat([dev_x_scaled_num, df_dev[feat_meta["cat"]].reset_index(drop=True)], axis=1)
+    test_x_scaled = pd.concat([test_x_scaled_num, df_test[feat_meta["cat"]].reset_index(drop=True)], axis=1)
+    
+    model_params = {
+            'objective': 'regression',
+            'metric': 'rmse',
+            'boosting_type': 'gbdt',
+            'learning_rate': 0.05,
+            'num_leaves': 31,
+            'max_depth': -1,
+            'min_data_in_leaf': 20,
+            'feature_fraction': 0.9,
+            'bagging_fraction': 0.8,
+            'bagging_freq': 5,
+            'categorical_feature': feat_meta["cat"],
+            'seed': 20250620,
+            'verbose': -1
+    }
+    result = lgb_train_model(train_x_scaled, train_y, df_train, test_x_scaled, test_y, df_test, dev_x_scaled, dev_y, df_dev, model_params, feat_meta["target"], checkpoint_name)
+
+    #evaluation metric
+
+    y_test = result["resale_price_raw"]
+    y_pred = result["predict"]
+    metric = {
+        "mae": mean_absolute_error(y_test, y_pred),
+        "rmse": root_mean_squared_error(y_test, y_pred),
+        "r2": r2_score(y_test, y_pred),
     }
 
-    grid_search = GridSearchCV(model, params, cv=5, scoring='neg_mean_squared_error')
-    grid_search.fit(X_train, y_train)
-    print(grid_search.best_params_)
+    #output save to db
+    conf = f"""http::addr={os.getenv("QDB_HOST")}:{os.getenv("QDB_PORT")};username={os.getenv("QDB_USER")};password={os.getenv("QDB_PWD")};""" 
 
-from sklearn.metrics import mean_absolute_error, root_mean_squared_error, r2_score
+    hdb_resale_price_predict_tb = "hdb_resale_predict_price"   
+    hdb_resale_price_experiment_meta_tb = "hdb_resale_predict_price_meta"   
+    with Sender.from_conf(conf) as sender:
+        result["testid"] = testid
+        sender.dataframe(result, table_name = hdb_resale_price_predict_tb, at = TimestampNanos.now())
+        sender.flush() 
 
-y_pred = model.predict(X_test)
-print("MAE:", mean_absolute_error(y_test, y_pred))
-print("RMSE:", root_mean_squared_error(y_test, y_pred, squared=False))
-print("R²:", r2_score(y_test, y_pred))
+        dd = deepcopy(model_params)
+        dd["categorical_feature"] = json.dumps(dd["categorical_feature"])
+        dd = dict(map(lambda x: (x[0], [x[1]]), dd.items()))
+        dd["feature_meta"] = json.dumps(feat_meta)
+        dd["evaluate_metric"] = json.dumps(metric)
+        dd["testid"] = [testid]
+        dd = pd.DataFrame(dd)
+        sender.dataframe(dd, table_name = hdb_resale_price_experiment_meta_tb, at = TimestampNanos.now())
+        sender.flush() 
+
+
+if __name__ == "__main__":
+    train_eval()
+
 
